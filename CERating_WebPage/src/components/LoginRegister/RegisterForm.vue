3a3cd23788c141e006a50b258595602dcc5d2411
LightInk2020 <crescent2020@163.com>
2022-01-24 10:40:46 +0800

    change login and register

---
+++ CERating_WebPage/src/components/LoginRegister/RegisterForm.vue
@@ -0,0 +1,267 @@
+<template>
+  <div>
+    <el-form
+      class="register-form"
+      ref="ref-registerform"
+      :model="registerForm"
+      :rules="registerRules"
+    >
+      <h3>注册</h3>
+      <el-form-item prop="businessName">
+        <input type="text" v-model="registerForm.businessName" required />
+        <div class="input-underline"></div>
+        <label class="input-label">企业名称</label>
+      </el-form-item>
+      <el-form-item prop="businessType1">
+        <div class="businessType1-box">
+          <label>企业类型</label>
+          <select name="businessType1">
+            <option value="industry">工业</option>
+            <option value="agriculture">农业</option>
+            <option value="business">商业</option>
+            <option value="tourism">旅行业</option>
+          </select>
+        </div>
+      </el-form-item>
+      <el-form-item prop="businessType2">
+        <div class="businessType2-box">
+          <div>
+            <input
+              type="radio"
+              name="businessType2"
+              id="type2-center"
+              value="center"
+            />
+            <label for="type2-center">总公司</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="businessType2"
+              id="type2-brach"
+              value="brach"
+            />
+            <label for="type2-brach">分公司</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="businessType2"
+              id="type2-other"
+              value="other"
+            />
+            <label for="type2-other">其他</label>
+          </div>
+        </div>
+      </el-form-item>
+      <el-form-item prop="email">
+        <input type="text" v-model="registerForm.email" required />
+        <div class="input-underline"></div>
+        <label class="input-label">邮箱</label>
+      </el-form-item>
+      <el-form-item prop="password">
+        <input type="password" v-model="registerForm.password" required />
+        <div class="input-underline"></div>
+        <label class="input-label">密码</label>
+      </el-form-item>
+      <el-form-item prop="securityCode" inline="true" class="security-code-box">
+        <input
+          type="text"
+          autocomplete="false"
+          v-model="registerForm.securityCode"
+          style="width: 58%"
+          required
+        />
+        <div class="input-underline"></div>
+        <label class="input-label">验证码</label>
+        <el-button
+          class="security-code-button"
+          type="primary"
+          @click="getSecurityCode"
+          >获取验证码</el-button
+        >
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitregister">注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+  data() {
+    return {
+      securityCodeUrl: "",
+      registerForm: {
+        businessName: "",
+        businessType1: "",
+        businessType2: "",
+        email: "",
+        password: "",
+        securityCode: "",
+      },
+    };
+  },
+  methods: {
+    getSecurityCode() {
+      alert("获取验证码");
+    },
+    submitregister() {
+      alert("register");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.register-form {
+  margin: 0 0.6rem;
+}
+.register-form h3 {
+  text-align: center;
+  font-size: 2.3rem;
+  padding-bottom: 15px;
+  width: 100%;
+  /* 大写 */
+  text-transform: uppercase;
+}
+.register-form input {
+  font-size: 1rem;
+  background: rgba(39, 39, 41, 0.025);
+  /* background: rgba(255, 255, 255, 0); */
+  border-radius: 0.5rem;
+  padding-left: 1rem;
+  /* border: 1px solid transparent; */
+  border: none;
+  height: 46px;
+  width: 100%;
+  line-height: 150%;
+  color: #25262b;
+}
+.register-form input:focus {
+  /* border: rgb(180, 193, 255) solid 1px; */
+  outline: none;
+  background: rgba(255, 255, 255, 0.04);
+}
+.register-form label {
+  font-size: 1rem;
+  position: absolute;
+  bottom: 10px;
+  left: 1rem;
+  color: rgba(65, 65, 65, 0.6);
+  pointer-events: none;
+  transition: all 0.3s ease;
+}
+.businessType1-box {
+  position: relative;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 35px;
+  line-height: 30px;
+}
+.businessType1-box label {
+  position: relative;
+  left: 0;
+  bottom: 0;
+}
+.businessType1-box select {
+  /*清除select的边框样式*/
+  border: none;
+  /*清除select聚焦时候的边框颜色*/
+  outline: none;
+  /*将select的宽高等于div的宽高*/
+  width: 60%;
+  text-align: center;
+  color: rgba(65, 65, 65, 0.8);
+  font-size: 1rem;
+  background: rgba(39, 39, 41, 0.025);
+}
+.businessType2-box {
+  display: flex;
+  width: 100%;
+  height: 30px;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+  align-items: center;
+}
+.businessType2-box label {
+  position: relative;
+  pointer-events: fill;
+  bottom: 0;
+  font-size: 1rem;
+  margin-left: 0;
+  bottom: 2px;
+}
+.businessType2-box input {
+  height: 1rem;
+  width: 1rem;
+}
+.businessType2-box span {
+  font-size: 1rem;
+  line-height: 1rem;
+}
+/* 关联input与label  input的focus触发会改变label的属性  */
+.register-form input:focus ~ .input-label,
+.register-form input:valid ~ .input-label {
+  /* :valid选择器是如果合法触发，所以输入框记得加required属性 */
+  transform: translateY(-1.8rem) translateX(-0.5rem);
+  font-size: 0.85rem;
+  letter-spacing: 0.05rem;
+  /* color: rgba(75, 105, 254, 0.7); */
+}
+.register-form .input-underline {
+  position: absolute;
+  bottom: 0px;
+  height: 1.4px;
+  width: 95%;
+  left: 2.5%;
+}
+.register-form .security-code-box .input-underline {
+  width: 55%;
+}
+/* 不理解这里加before选择器是为了什么 */
+.register-form .input-underline:before {
+  position: absolute;
+  content: "";
+  height: 100%;
+  width: 100%;
+  /* background: rgba(75, 105, 254, 0.7); */
+  background: rgba(129, 129, 129, 0.3);
+  transform: scaleX(0);
+  transition: transform 0.3s ease;
+}
+.register-form input:focus ~ .input-underline:before {
+  transform: scaleX(1);
+}
+.register-form button {
+  width: 100%;
+  background-image: linear-gradient(
+    130deg,
+    rgb(75, 105, 254) 0%,
+    rgba(150, 152, 253, 0.75) 100%
+  );
+  border-radius: 10px;
+  backdrop-filter: blur(24px);
+  border: none;
+  cursor: pointer;
+  height: 48px;
+  font-size: 16px;
+}
+.register-form .security-code-box >>> div {
+  display: flex;
+  justify-content: space-between;
+}
+.register-form .security-code-button {
+  background-image: linear-gradient(
+    130deg,
+    #93a8f1b6 0%,
+    rgba(152, 171, 255, 0.6) 100%
+  );
+  font-size: 0.9rem;
+  width: 35%;
+}
+</style>
--- CERating_WebPage/src/views/LoginRegister.vue
+++ CERating_WebPage/src/views/LoginRegister.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="context">
-    <div class="con-img"></div>
+    <div class="con-background"></div>
     <div class="left-container">
       <div class="logo-box">
         <img src="../assets/logo.png" alt="" />
@@ -12,180 +12,64 @@
       </div>
       <div class="footer-box">
         <span class="footer-list">
-          <a class="footer-about" target="_blank" href="/about"> 关于我们 </a>
+          <a class="footer-about" target="_blank" href="/about">关于</a>
           <span class="footer-line"></span>
           <a class="footer-help" target="_blank" href="">帮助</a>
         </span>
         <span class="footer-copyright"> 2022 ©CERating </span>
       </div>
     </div>
-    <div class="container">
-      <div class="form-box">
-        <!-- 注册 -->
-        <div class="register-box hidden">
-          <h1>register</h1>
-          <el-form
-            ref="refRegisterForm"
-            :model="registerForm"
-            :rules="rulesRegister"
-            class=""
-          >
-            <el-form-item prop="username">
-              <el-input
-                v-model="registerForm.username"
-                placeholder="Username"
-                class="lr-input"
-              ></el-input>
-            </el-form-item>
-            <el-form-item prop="email">
-              <el-input
-                v-model="registerForm.email"
-                placeholder="Email"
-                class="lr-input"
-              ></el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input
-                v-model="registerForm.password"
-                type="password"
-                placeholder="Password"
-                class="lr-input"
-              ></el-input>
-            </el-form-item>
-            <el-form-item prop="checkpassword">
-              <el-input
-                v-model="registerForm.checkpassword"
-                type="password"
-                placeholder="Enter your password again"
-                class="lr-input"
-              ></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button
-                @click="handleRegister('refRegisterForm')"
-                type="primary"
-                >register</el-button
-              >
-            </el-form-item>
-          </el-form>
-        </div>
-        <!-- 登录 -->
-        <div class="login-box">
-          <h1>login</h1>
-          <el-form
-            ref="refLoginForm"
-            :model="loginForm"
-            :rules="rulesLogin"
-            class=""
-          >
-            <el-form-item prop="account">
-              <el-input
-                v-model="loginForm.account"
-                placeholder="Account"
-                class="lr-input"
-              ></el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input
-                v-model="loginForm.password"
-                type="password"
-                placeholder="Password"
-                class="lr-input"
-              ></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button @click="handleLogin('refLoginForm')" type="primary"
-                >login</el-button
-              >
-            </el-form-item>
-          </el-form>
-        </div>
+    <div class="right-container">
+      <div class="right-context">
+        <!-- 点击按钮会显示不同组件 -->
+        <component :is="loginOrRegister"></component>
       </div>
-      <div class="con-box left">
-        <h2>欢迎来到</h2>
-        <h2><span>碳排放评测系统</span></h2>
-        <p>来实时测评<span>碳排放</span>吧</p>
-        <!-- <video src="../assets/login_register.mp4" muted loop autoplay></video> -->
-        <img src="../assets/login_1.jpeg" alt="" />
-        <p>已有账户？</p>
-        <button id="login" @click="tologin">去登录</button>
-      </div>
-      <div class="con-box right">
-        <h2>欢迎来到</h2>
-        <h2><span>碳排放评测系统</span></h2>
-        <p>来试一下测评<span>碳排放等级</span>吧</p>
-        <!-- <video src="../assets/login_register.mp4" muted loop autoplay></video> -->
-        <img src="../assets/login_2.jpeg" alt="" />
-        <p>没有账户？</p>
-        <button id="register" @click="toregister">去注册</button>
+      <div class="right-footer-box">
+        <div class="right-footer-login-box">
+          <a class="forget-password" href="/about">忘记密码</a>
+          <span class="footer-line"></span>
+          <a class="quick-register" @click="toRegister">快速注册</a>
+        </div>
+        <div class="right-footer-register-box" style="display:none;">
+          <span>已有账户？</span>
+          <a class="quick-login" @click="toLogin">去登录</a>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
-import { getCurrentInstance } from "vue";
-import { loginForm, rulesLogin } from "@/utils/loginValidators";
-import { registerForm, rulesRegister } from "@/utils/registerValidators";
+import LoginForm from "@/components/LoginRegister/LoginForm.vue";
+import RegisterForm from "@/components/LoginRegister/RegisterForm.vue";
+
 export default {
   name: "LoginRegister",
-  components: {},
-  setup() {
-    const { ctx } = getCurrentInstance();
-
-    // 登录/注册触发所有检验
-    const handleRegister = (formName) => {
-      ctx.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          alert("error submit!");
-          return false;
-        }
-      });
-    };
-    const handleLogin = (formName) => {
-      ctx.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          alert("error submit!");
-          return false;
-        }
-      });
-    };
-
+  components: {
+    LoginForm,
+    RegisterForm,
+  },
+  data() {
     return {
-      loginForm,
-      registerForm,
-      rulesRegister,
-      rulesLogin,
-      handleRegister,
-      handleLogin,
+      loginOrRegister: "LoginForm",
     };
   },
   methods: {
-    tologin() {
-      document.getElementsByClassName("form-box")[0].style.transform =
-        "translateX(0%)";
-      document
-        .getElementsByClassName("register-box")[0]
-        .classList.add("hidden");
-      document
-        .getElementsByClassName("login-box")[0]
-        .classList.remove("hidden");
+    toRegister() {
+      document.getElementsByClassName("right-footer-login-box")[0].setAttribute("style","display:none");
+      document.getElementsByClassName("right-footer-register-box")[0].setAttribute("style","display");
+      document.getElementsByClassName("right-container")[0].setAttribute("style", "height:500px; margin-top: -290px");
+      this.loginOrRegister = "RegisterForm";
     },
-    toregister() {
-      document.getElementsByClassName("form-box")[0].style.transform =
-        "translateX(80%)";
-      document.getElementsByClassName("login-box")[0].classList.add("hidden");
-      document
-        .getElementsByClassName("register-box")[0]
-        .classList.remove("hidden");
+    toLogin() {
+      document.getElementsByClassName("right-footer-register-box")[0].setAttribute("style","display:none");
+      document.getElementsByClassName("right-footer-login-box")[0].setAttribute("style","display");
+      document.getElementsByClassName("right-container")[0].setAttribute("style", "height:350px; margin-top: -215px");
+      this.loginOrRegister = "LoginForm";
     },
   },
 };
 </script>
-
 
 <style scoped>
 .context {
@@ -193,15 +77,13 @@ export default {
   padding: 0;
   /* 100%窗口高度 */
   height: 100vh;
-  /* 弹性布局  水平+垂直居中 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  transition: all 0.5s;
+
   /* 渐变背景 */
   /* background: linear-gradient(200deg, #f3e7e9, #e3eeff); */
   /* background-image: url("../assets/login_background.jpg"); */
 }
-.con-img {
+.con-background {
   position: fixed;
   left: 0;
   right: 0;
@@ -228,17 +110,10 @@ export default {
   letter-spacing: 3.81px;
   font-weight: 300;
 }
-/* 窗口变化会导致text-box隐藏 */
-@media all and (max-width: 1000px) {
-  /* max screen size */
-  .text-box {
-    display: none;
-  }
-}
 .logo-box {
   position: fixed;
-  left: 7%;
-  top: 10%;
+  left: 6%;
+  top: 9%;
   color: #fff;
   text-align: left;
   display: flex;
@@ -247,9 +122,9 @@ export default {
   align-items: center;
 }
 .logo-box span {
-    margin-left: 10px;
-    font-size: 30px;
-    font-family: Times;
+  margin-left: 10px;
+  font-size: 30px;
+  font-family: Times;
 }
 .logo-box img {
   height: 50px;
@@ -258,22 +133,18 @@ export default {
 .footer-box {
   position: fixed;
   opacity: 0.78;
-  left: 8%;
-  bottom: 10%;
+  left: 6%;
+  bottom: 8%;
   font-size: 14px;
   color: #fff;
 }
 .footer-box a {
+  color: #fff;
   text-decoration: none;
 }
 .footer-box a:hover {
-  text-decoration: underline;
-  /* font-weight: bold; */
-  /* color: #BDCEFC; */
+  color: rgb(75, 105, 254);
   opacity: 1;
-}
-.footer-box a:visited {
-  color: #fff;
 }
 .footer-about .footer-help {
   opacity: 0.8;
@@ -299,164 +170,86 @@ export default {
   display: inline-block;
   padding-left: 30px;
 }
-.container {
+.right-container {
+  top: 50%;
+  margin-top: -215px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  right: 170px;
+  width: 420px;
+  height: 350px; /* login的高度 */
+  padding: 28px 35px 20px 35px;
   background: rgba(255, 255, 255, 0.9);
-  top: 16%;
-  right: 7.5%;
-  width: 660px;
-  height: 450px;
   border-radius: 12px;
   overflow: hidden;
-  /* 阴影 */
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
-  /* 相对定位 */
-  position: absolute;
+  transition: all 0.5s;
 }
-.form-box {
-  position: absolute;
-  left: 5%;
-  background: linear-gradient(0, #d0dcf0, #bdcefc);
-  width: 330px;
-  height: 450px;
-  /* border-radius: 5px; */
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 2;
-  /* 动画过渡 加速后减速 */
-  transition: 0.5s ease-in-out;
-}
-.form-box:hover {
-  box-shadow: 2px 0 5px 5px rgb(0 0 0 / 5%);
-  transition: 0.5s;
-}
-.form-box button {
-  width: 100%;
-  margin-top: 20px;
-  background-color: #f6f6f6;
-  outline: none;
-  border-radius: 5px;
-  padding: 13px;
-  color: #bdcefc;
-  letter-spacing: 2px;
-  border: none;
-  cursor: pointer;
-}
-.form-box button:hover {
-  background-color: #bdcefc;
-  color: #f6f6f6;
-  transition: background-color 0.5s ease;
-}
-.register-box,
-.login-box {
-  /* 弹性布局 垂直排列 */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  widows: 100%;
-}
-.hidden {
-  display: none;
-  transition: 0.5s;
-}
-h1 {
-  text-align: center;
-  margin-bottom: 25px;
-  /* 大写 */
-  text-transform: uppercase;
-  color: #fff;
-  /* 字间距 */
-  letter-spacing: 5px;
-}
-input {
-  background-color: transparent;
-  width: 70%;
-  color: #fff;
-  border: none;
-  /* 下边框样式 */
-  border-bottom: 1px solid rgba(255, 255, 255, 0.4);
-  padding: 10px 0;
-  text-indent: 10px;
-  margin: 8px 0;
-  font-size: 14px;
-  letter-spacing: 2px;
-}
-input::placeholder {
-  opacity: 0;
-}
-input:focus {
-  color: #bdcefc;
-  outline: none;
-  border-bottom: 1px solid #a9b9ff;
-  transition: 0.5s;
-}
-input:focus::placeholder {
-  opacity: 0;
-}
-.con-box {
-  width: 50%;
-  /* 弹性布局 垂直排列 居中 */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  /* 绝对定位 居中 */
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-}
-.con-box.left {
-  left: -2%;
-}
-.con-box.right {
-  right: -2%;
-}
-.con-box h2 {
-  color: #8e9aaf;
-  font-size: 18px;
-  font-weight: bold;
-  letter-spacing: 3px;
-  text-align: center;
-  margin-bottom: 4px;
-}
-.con-box p {
-  font-style: 10px;
-  letter-spacing: 2px;
-  color: #8e9aaf;
+.right-footer-box {
   text-align: center;
 }
-.con-box span {
-  color: #bdcefc;
+.right-footer-box a {
+  color: rgb(100, 100, 100);
+  text-decoration: none;
+  cursor: pointer; /* 光标变小手 */
 }
-.con-box video {
-  width: 280px;
-  height: 450px;
-  object-fit: cover;
-  opacity: 0.9;
-  margin: 40px 0;
-  position: fixed;
-  z-index: -1;
+.right-footer-box a:hover {
+  color: rgb(75, 105, 254);
+  opacity: 1;
 }
-.con-box img {
-  width: 200px;
-  height: 200px;
-  opacity: 0.9;
-  margin: 10px 0;
+/* 窗口变化会导致text-box隐藏 */
+@media screen and (max-width: 1000px) {
+  /* max screen size */
+  .text-box {
+    display: none;
+  }
 }
-.con-box button {
-  margin-top: 3%;
-  background-color: #fff;
-  color: #a9b9ff;
-  border: 1px solid #bdcefc;
-  padding: 6px 10px;
-  border-radius: 5px;
-  letter-spacing: 1px;
-  outline: none;
-  cursor: pointer;
+/* 窗口自适应  登录/注册窗口 */
+@media screen and (max-width: 1000px) {
+  .context {
+    animation: fadeIn 0.25s linear;
+    display: flex;
+    justify-content: center;
+  }
+  .right-container {
+    right: auto;
+  }
 }
-.con-box button:hover {
-  background-color: #bdcefc;
-  color: #fff;
+@media screen and (max-width: 550px) {
+  .right-container {
+    background-color: transparent !important;
+    padding: 0 0;
+    height: auto;
+    width: 70%;
+  }
+  .con-background {
+    background: #fff;
+    background-size: cover;
+  }
+  .logo-box {
+    display: none;
+  }
+  .footer-box {
+    position: absolute;
+    left: 41%;
+    width: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    overflow: hidden;
+    transition: all 0.5s;
+  }
+  .footer-box a {
+    color: rgb(0, 0, 0);
+  }
+  .footer-copyright {
+    color: rgb(100, 100, 100);
+    padding-top: 0.3rem;
+    padding-left: 2px;
+  }
+  .footer-line {
+    background: rgb(100, 100, 100);
+  }
 }
 </style>
