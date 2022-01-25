9bfe0e776707a03896bcb03de3d4ccded27caef9
Evil Hare <eharecz@gmail.com>
2022-01-25 11:52:41 +0800

    after merge 4 works

--- CERating_Server/CERating_Server/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-def hello(request):
-    return  render(request,'enterprise_register/login.html')
-
